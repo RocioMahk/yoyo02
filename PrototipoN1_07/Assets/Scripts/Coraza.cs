4fea767df648d3a53a877fbcfe3bc4b8326a8372
Luis Ramirez <luis.e.3194@gmail.com>
2018-03-29 18:01:26 -0600

    cosa2

---
+++ PrototipoN1_07/Assets/Scripts/Coraza.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coraza : State {
+	
+	public State correr;
+	public Color alertColor;
+
+	private Color normalColor;
+
+
+	void Start(){
+	}
+
+	void OnEnable()
+	{
+		normalColor = GetComponent<Renderer>().material.color;
+	}
+
+	void Update()
+	{
+		Debug.Log ("entre a coraza");
+		if (GetComponent<Renderer>().material.color.Equals(normalColor)) { GetComponent<Renderer>().material.color = alertColor; }
+		else { GetComponent<Renderer>().material.color = normalColor; }
+	}
+
+	public override void CheckExit()
+	{
+		StartCoroutine (esperar1Seg ());
+	}
+
+	IEnumerator esperar1Seg()
+	{
+		yield return new WaitForSecondsRealtime (10);
+		stateMachine.ChangeState (correr);
+	}
+
+}
---
+++ PrototipoN1_07/Assets/Scripts/Morir.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Morir : State {
+
+	public float vida;
+
+	void Update()
+	{
+		if (vida == 0) {
+			Debug.Log ("falleci");
+			Destroy (this.gameObject);
+		}
+	}
+		
+}
